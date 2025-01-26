e667d622ef381f3f215d7753daf3a84512f9e3ba
Suleiman Yahaya <132784747+drkveil@users.noreply.github.com>
2025-01-26 15:55:18 +0100

    Get the computer's choice

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 console.log("Welcome To RPS");
+
+function getComputerChoice(){
+    let randomNumber = Math.floor((Math.random()) * 3);
+    return randomNumber;
+}
+
+if (getComputerChoice() === 1){
+    console.log("Rock");
+}else if (getComputerChoice() === 2){
+    console.log("Paper");
+}else{
+    console.log("Scissors");
+}
